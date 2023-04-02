d3a2ad2696827e42bf6e99b4a4d71c77f5e6f583
Nikos <nikosant82@gmail.com>
2023-04-02 11:48:24 +0200

    Vreate urls, views and templates for secureapp

--- webchecklist/secureapp/views.py
+++ webchecklist/secureapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+
+    return render(request, 'index.html')
+
+
+def register(request):
+
+    return render(request, 'register.html')
+
+
+def dashboard(request):
+
+    return render(request, 'dashboard.html')
